2565a28ac68b1a89c9f581a467621995b846d2bb
Abhibhaw <asthana3110@gmail.com>
2021-05-01 21:31:18 +0530

    Adds Model Function on Click

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request, Response, redirect, url_for
 
+import cv2
 from eval import captionize
-from capture import CaptureNPredict
 from utils import *
+from PIL import Image
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from caption_model import caption_model
+import numpy as np
+import pickle
+from process_data import encodeImage
+from gtts import gTTS   
+import os
+import requests
+from io import BytesIO
+from PIL import Image, ImageFile
 
 app = Flask(__name__)
 
@@ -18,11 +29,24 @@ def index():
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
-    if(request.method == 'POST'):
+    
+    cap = cv2.VideoCapture(f"{video_url}")
+    while True:
+        ret,frame = cap.read()
+        w,h,l = frame.shape
+        dim = (int(w/3),int(h/3))
+        #frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+        cv2.imshow("capturing",frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if(request.method == 'POST'):
+            cv2.imwrite(input_path,frame)
+            captionize(input_path, input_path,play_now=True,print_caption=True)
 
-        CaptureNPredict(img_path = input_path, sound_path = output_path ,server_address = video_url)
+    cap.release()
+    cv2.destroyAllWindows()
 
-        return render_template('result.html')
+    return render_template('result.html')
 
 
 if __name__ == "__main__":
